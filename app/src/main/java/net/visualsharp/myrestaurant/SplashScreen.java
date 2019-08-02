3263d43fe8a3e70c61f7136cbb154167f58feb89
Thinh Tran <thinhtran@outlook.com.au>
2019-08-02 22:05:10 +0800

    Add Phone number Login with SMS code using Facebook-Account-Kit-SDK

--- app/src/main/java/net/visualsharp/myrestaurant/SplashScreen.java
+++ app/src/main/java/net/visualsharp/myrestaurant/SplashScreen.java
@@ -3,14 +3,25 @@ package net.visualsharp.myrestaurant;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Message;
+import android.util.Base64;
+import android.util.Log;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class SplashScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+   //     printKeyHash(); //Only run this first time to get HashKey for Facebook Account Kit setup
 
         new Handler().postDelayed(new Runnable() {
             @Override
@@ -19,4 +30,23 @@ public class SplashScreen extends AppCompatActivity {
             }
         },3000);
     }
+
+//    private void printKeyHash() {
+//        try{
+//            PackageInfo info = getPackageManager().getPackageInfo(getPackageName(),
+//                    PackageManager.GET_SIGNATURES);
+//            for(Signature signature:info.signatures)
+//            {
+//                MessageDigest md = MessageDigest.getInstance("SHA");
+//                md.update(signature.toByteArray());
+//                Log.d("KEY_HASH", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+//            }
+//        }
+//        catch (PackageManager.NameNotFoundException e){
+//            e.printStackTrace();
+//        }
+//        catch (NoSuchAlgorithmException e){
+//
+//        }
+//    }
 }
