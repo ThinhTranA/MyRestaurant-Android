9fabca3188451879792a3a872fe010de1f7f1c76
Thinh Tran <thinhtran@outlook.com.au>
2019-08-03 13:57:10 +0800

    Implemented  similar task from SplashScreen in MainActivity when user Login success from Account Kit

--- app/src/main/java/net/visualsharp/myrestaurant/MainActivity.java
+++ app/src/main/java/net/visualsharp/myrestaurant/MainActivity.java
@@ -3,23 +3,39 @@ package net.visualsharp.myrestaurant;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.facebook.accountkit.Account;
+import com.facebook.accountkit.AccountKit;
+import com.facebook.accountkit.AccountKitCallback;
+import com.facebook.accountkit.AccountKitError;
 import com.facebook.accountkit.AccountKitLoginResult;
 import com.facebook.accountkit.LoginResult;
 import com.facebook.accountkit.ui.AccountKitActivity;
 import com.facebook.accountkit.ui.AccountKitConfiguration;
 import com.facebook.accountkit.ui.LoginType;
 
+import net.visualsharp.myrestaurant.Common.Common;
+import net.visualsharp.myrestaurant.Retrofit.IMyRestaurantAPI;
+import net.visualsharp.myrestaurant.Retrofit.RetrofitClient;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import dmax.dialog.SpotsDialog;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
+    IMyRestaurantAPI myRestaurantAPI;
+    CompositeDisposable compositeDisposable = new CompositeDisposable();
+    AlertDialog dialog;
     private static final int APP_REQUEST_CODE = 1234;
 
     @BindView(R.id.btn_sign_in)
@@ -44,7 +60,42 @@ public class MainActivity extends AppCompatActivity {
             }
             else
             {
-                Toast.makeText(this, "Success Login", Toast.LENGTH_SHORT).show();
+                //Login Success
+                dialog.show();
+                AccountKit.getCurrentAccount(new AccountKitCallback<Account>() {
+                    @Override
+                    public void onSuccess(Account account) {
+                        compositeDisposable.add(myRestaurantAPI.getUser(Common.API_KEY, account.getId())
+                                .subscribeOn(Schedulers.io())
+                                .observeOn(AndroidSchedulers.mainThread())
+                                .subscribe(userModel -> {
+
+                                    //If user already in database
+                                        if(userModel.isSuccess())
+                                        {
+                                            Common.currentUser = userModel.getResult().get(0);
+                                            Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                                            startActivity(intent);
+                                            finish();
+                                        }
+                                        else{ // If user not register
+                                            Intent intent = new Intent(MainActivity.this, UpdateInfoActivity.class);
+                                            startActivity(intent);
+                                            finish();
+                                        }
+                                        dialog.dismiss();
+                                        },
+                                        throwable -> {
+                                            dialog.dismiss();
+                                            Toast.makeText(MainActivity.this,"[GET USER] "+ throwable.getMessage(),Toast.LENGTH_SHORT).show();
+                                        }));
+                    }
+
+                    @Override
+                    public void onError(AccountKitError accountKitError) {
+                        Toast.makeText(MainActivity.this, "[ACCOUNT KIT ERROR]"+accountKitError.getErrorType().getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
             }
         }
     }
@@ -61,10 +112,23 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onDestroy() {
+        compositeDisposable.clear();
+        super.onDestroy();
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         ButterKnife.bind(this);
+
+        init();
+    }
+
+    private void init() {
+        dialog = new SpotsDialog.Builder().setContext(this).setCancelable(false).build();
+        myRestaurantAPI = RetrofitClient.getInstance(Common.API_RESTAURANT_ENDPOINT).create(IMyRestaurantAPI.class);
     }
 }
